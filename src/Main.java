8104d19eeb52d218482d1ced138488fbc79eced9
Aslan <128255111+AslanAslan01@users.noreply.github.com>
2023-05-17 13:01:33 -0400

    Delete src directory

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-
-        System.out.println("I am a project");
-        System.out.println("I am a continuation to project");
-    }
-}
